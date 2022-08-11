5b6392a73459a72acbfdc2f7f1fd7ce2a1d8b53c
Ojongseok <whdtjr6889@naver.com>
2022-08-11 16:05:35 +0900

    08.11 16:05

--- app/src/main/java/com/example/a22_2_sejong_project/MainActivity.kt
+++ app/src/main/java/com/example/a22_2_sejong_project/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.a22_2_sejong_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.a22_2_sejong_project.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),BottomNavigationView.OnNavigationItemSelectedListener {
+    private var _Binding: ActivityMainBinding? = null
+    private val binding get() = _Binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _Binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainBottomNav.setOnNavigationItemSelectedListener(this)
+        binding.mainBottomNav.selectedItemId = R.id.nav_item1
+
+
+    }
+    override fun onDestroy() {
+        _Binding = null
+        super.onDestroy()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+
+        return false
     }
 }
