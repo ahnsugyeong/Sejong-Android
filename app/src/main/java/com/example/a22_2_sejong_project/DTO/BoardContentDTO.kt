34e06edc1aa65344717da27138e4d09a666c8ed3
suddiyo <suddii01@sju.ac.kr>
2022-09-15 19:03:18 +0900

    update item_board_main.xml

--- app/src/main/java/com/example/a22_2_sejong_project/DTO/BoardContentDTO.kt
+++ app/src/main/java/com/example/a22_2_sejong_project/DTO/BoardContentDTO.kt
@@ -4,10 +4,12 @@ data class BoardContentDTO(
     var title: String? = null,
     var description: String? = null,
     var uid: String? = null,
-    var userId: String? = null,
+    var userId: String? = null, // 이용해서 profile image, nickname 뽑아오기
     var timestamp: Long? = null,
     var favoriteCount: Int = 0,
     var commentCount: Int = 0,
+    var currentNumber: Int? = null, // 현재 모집된 인원
+    var totalNumber: Int? = null,   // 총 모집할 인원
     var favorites: MutableMap<String, Boolean> = HashMap()
 ) {
     data class Comment(
